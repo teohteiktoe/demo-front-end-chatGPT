1ac05e0db701b9763c09b564e23290fafc7261d7
Teoh TeikToe <teohteiktoe@gmail.com>
2024-08-04 17:45:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 from flask import request, render_template
 
+import google.generativeai as palm
+
+palm.configure(api_key="AIzaSyCaQcgKn95ZO6AR1t2PXzk9UydTkt4sWZQ")
+
+model = { 'model': "models/chat-bison-001"}
+
 @app.route("/", methods = ["GET", "POST"])
 def index():
     return render_template("index.html")
@@ -10,6 +16,21 @@ def index():
 def prediction():
     num = float(request.form.get("rates"))
     return(render_template("prediction.html", r = 90.2-(50.6*num)))
+
+@app.route("/genAI", methods = ["GET", "POST"])
+def genAI():
+    return render_template("genAI.html")
+
+@app.route("/genAI_reply", methods = ["GET", "POST"])
+def genAI_reply():
+    q = request.form.get("q")
+    print("in makersuite")
+    r = palm.chat(
+        **model,
+        messages=q
+    )
+    print("done makersuite")
+    return render_template("genAI_reply.html",r=r.last)
 
 if __name__=="__main__":
     app.run()
